9bb1d4a6239c0b2e46dcef967dbdbb7cc159661f
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-07-07 19:24:53 +0300

    map edit

---
+++ index.js
@@ -0,0 +1,15 @@
+function aboutfunc(){
+    document.documentElement.scrollTop= 2500;
+}
+
+hams= document.querySelector('.hams');
+
+navbar= document.querySelector('.navbar');
+
+navitems= document.querySelector('.navitems');
+
+hams.addEventListener('click', ()=>{
+    navbar.classList.toggle('h-nav');
+    navitems.classList.toggle('navseen');
+})
+
